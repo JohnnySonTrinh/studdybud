ebc7358a407147b3cb76e27466e73549f6aebd9e
Johnny Trinh <johnny.trinh@hotmail.se>
2024-02-08 20:14:33 +0100

    Add activity feed to mobil menu

--- base/views.py
+++ base/views.py
@@ -195,3 +195,7 @@ def topicsPage(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
     topics = Topic.objects.filter(name__icontains=q)
     return render(request, 'base/topics.html', {'topics': topics})
+
+def activityPage(request):
+    room_messages = Message.objects.all().order_by('-created')
+    return render(request, 'base/activity.html', {'room_messages': room_messages})
